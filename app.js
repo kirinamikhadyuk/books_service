a0926e5fa9b80f8687e3e83f6ea3b472b57ae8ee
Kirina Mikhadyuk <mikhadyuk@gmail.com>
2022-11-23 17:40:41 +0300

    changes

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
 const model = require('./model/book');
-// const cors = require('cors');
 require('dotenv').config();
 
 const mongoURL = process.env.MONGO_URL;
@@ -13,14 +12,7 @@ mongoose.connect(mongoURL);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-/*app.use(cors({
-    origin: "https://form-mikhadyuk.herokuapp.com",
-    methods: "GET,POST,DELETE",
-    preflightContinue: false,
-    credentials: true
-}));*/
 
-//Cors Configuration - Start
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,POST,DELETE");
@@ -33,14 +25,12 @@ app.use(function(req, res, next) {
     }
     next();
 });
-//Cors Configuration - End
 
 app.get("/", (req, res) => {
     res.send("This is books service");
 });
 
 app.post("/book", (req, res) => {
-    console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
     const book = new Book(req.body);
     book.save()
         .then(data => {
